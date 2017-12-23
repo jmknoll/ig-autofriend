9c94b813774c84208a52db0f5e275910b56ed67f
Jameson Knoll <j.knoll621@gmail.com>
2017-12-23 11:53:00 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+//todo
+
+figure out how class-based structure works and be able to run your program
+docker
+.env variables
---
+++ src/InstaFriend.py
@@ -0,0 +1,9 @@
+class InstaFriend:
+	
+	def __init__(self):
+		self.title = "bonesaw"
+
+	def say_something(self):
+		print(self.title)
+
+  say_something()
